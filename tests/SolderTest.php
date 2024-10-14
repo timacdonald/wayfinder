50974658db8c0741abfc5c15a305c42cfd9ca0fb
Tim MacDonald <hello@timacdonald.me>
2024-10-15 08:51:35 +1100

    wip

--- tests/SolderTest.php
+++ tests/SolderTest.php
@@ -6,6 +6,8 @@ class SolderTest extends TestCase
 {
     public function test_it_works()
     {
+        $this->artisan('solder:generate');
+
         $this->assertTrue(true);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,8 +4,16 @@ namespace Tests;
 
 use Orchestra\Testbench\Concerns\WithWorkbench;
 use Orchestra\Testbench\TestCase as BaseTestCase;
+use TiMacDonald\Solder\SolderServiceProvider;
 
 class TestCase extends BaseTestCase
 {
     use WithWorkbench;
+
+    protected function getPackageProviders($app)
+    {
+        return [
+            SolderServiceProvider::class,
+        ];
+    }
 }
