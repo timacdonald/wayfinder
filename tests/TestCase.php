7ab2bc641892e12fdfe8f708134a24e037c7c2ae
Tim MacDonald <hello@timacdonald.me>
2024-10-15 08:35:10 +1100

    wip

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,3 +1,12 @@
 <?php
 
 namespace Tests;
+
+use Orchestra\Testbench\Concerns\WithWorkbench;
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+    use WithWorkbench;
+}
+
