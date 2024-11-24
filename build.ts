bc7ceca5acc2031da76659a72ce57bd1066ba3c9
Tim MacDonald <hello@timacdonald.me>
2024-11-25 08:15:48 +1100

    wip

--- build.ts
+++ build.ts
@@ -1,5 +1,9 @@
 import { execSync } from 'node:child_process'
 
 export default function setup() {
-    execSync('vendor/bin/testbench wayfinder:generate --base=workbench/resources/js')
+    try {
+        execSync('vendor/bin/testbench wayfinder:generate --base=workbench/resources/js')
+    } catch (error) {
+        throw new Error(`Unable to generate wayfinder routes. Error: \n----------${error.output}\n----------`)
+    }
 }
